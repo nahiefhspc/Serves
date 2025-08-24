88a24004a35be37fffb7fdd04923eb83c2062dd6
nahiefhspc <t.al.khackheist@gmail.com>
2025-08-24 05:39:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,37 +6,36 @@ import requests
 app = Flask(__name__)
 
 SERVERS = {
-    "BOT 1": "https://bot1server.koyeb.app/",
-    "BOT 2": "https://bot2server.koyeb.app/",
-    "SERVER": "https://prime-robby-bedicjjwsh-132a0edd.koyeb.app/",
-    "MOTION": "https://mute-mommy-brcjjejdj-0be63631.koyeb.app/",
-    "Leech1": "https://concerned-ellyn-goejdj-c75adff2.koyeb.app/",
-    "Leech2": "https://absent-magdalen-ywdjcjs-ba359b35.koyeb.app/",
-    "Leech3": "https://pregnant-thrush-qfkcsjjc-ebbb9db4.koyeb.app/",
-    "Leech4": "https://current-sherilyn-wsdksk-454c250b.koyeb.app/",
-    "Signup": "https://steady-darelle-qxckehc-2bf9bb53.koyeb.app/",
-    "DreamMit": "https://prominent-julianna-brxwsx-867f3a56.koyeb.app/",
-    "2ndaccount": "https://molecular-sondra-qcvosch-b97baa7f.koyeb.app/",
-    "3602": "https://ministerial-rosalynd-brddxkj-321723bf.koyeb.app/",
-    "3603": "https://vital-junie-ckejcjdjwj-419a3721.koyeb.app/",
-    "saver": "https://protectorsop-2.onrender.com",
-    "Txt": "https://txesc-4rql.onrender.com",
-    "Txt2": "https://txesc-2.onrender.com",
-    "Txt3": "https://txesc-1.onrender.com",
-    "Txt4": "https://lulilele-3pci.onrender.com",
-    "Txt5": "https://lulilele-1-1ryu.onrender.com",
-    "Txt6": "https://upelckjwh.onrender.com",
-    "Txt7": "https://protectorsop-1.onrender.com",
-    "txt8": "https://txesc-mm88.onrender.com",
-    "Server1": "https://serves.onrender.com",
-    "PWEXTRACTOR": "https://protectorsop-4ulv.onrender.com",
-    "SAMX1": "https://txesc-p3oa.onrender.com",
-    "SAMX2": "https://txesc-79d0.onrender.com",
-    "SAMX3": "https://txesc-vqbm.onrender.com",
-    "SAMX4": "https://txesc-4jbw.onrender.com",
-    "SAMX5": "https://txesc-rbe3.onrender.com",
-    "SAMX6": "https://txesc-osdx.onrender.com",
-    "SERVER2": "https://gorgeous-ange-opechwxh-47aa648d.koyeb.app/",
+    "Jaggo": "https://jaggo.onrender.com/",
+    "SAMX 3": "https://txesc-vqbm-vnkh.onrender.com",
+    "7th": "https://txesc-o52c.onrender.com",
+    "Tryterbit": "https://txesc-phy7.onrender.com",
+    "Savers": "https://txesc-iesl.onrender.com",
+    "SAMX 2": "https://txesc-80ix.onrender.com",
+    "4th": "https://txesc-aopv.onrender.com",
+    "2nd": "https://txesc-8ejn.onrender.com",
+    "3rd": "https://txesc-1-s62m.onrender.com",
+    "10th": "https://txesc-1-gq8v.onrender.com",
+    "Jaggo": "https://jaggo.onrender.com/",
+    "SAMX 3": "https://txesc-vqbm-vnkh.onrender.com",
+    "7th": "https://txesc-o52c.onrender.com",
+    "Tryterbit": "https://txesc-phy7.onrender.com",
+    "Savers": "https://txesc-iesl.onrender.com",
+    "SAMX 2": "https://txesc-80ix.onrender.com",
+    "4th": "https://txesc-aopv.onrender.com",
+    "2nd": "https://txesc-8ejn.onrender.com",
+    "3rd": "https://txesc-1-s62m.onrender.com",
+    "10th": "https://txesc-1-gq8v.onrender.com",
+    "Jaggo": "https://jaggo.onrender.com/",
+    "SAMX 3": "https://txesc-vqbm-vnkh.onrender.com",
+    "7th": "https://txesc-o52c.onrender.com",
+    "Tryterbit": "https://txesc-phy7.onrender.com",
+    "Savers": "https://txesc-iesl.onrender.com",
+    "SAMX 2": "https://txesc-80ix.onrender.com",
+    "4th": "https://txesc-aopv.onrender.com",
+    "2nd": "https://txesc-8ejn.onrender.com",
+    "3rd": "https://txesc-1-s62m.onrender.com",
+    "10th": "https://txesc-1-gq8v.onrender.com",
 }
 
 def send_requests():
@@ -48,7 +47,7 @@ def send_requests():
             except Exception as e:
                 print(f"{bot_name} - Error: {e}")
         
-        time.sleep(1)  # Wait 60 seconds before sending the next batch
+        time.sleep(0.1)  # Wait 60 seconds before sending the next batch
 
 # Start the background thread
 thread = threading.Thread(target=send_requests, daemon=True)
